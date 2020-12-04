8a84fd2a1d39aa61c300435253a263b3ef961558
Jennifer Tracy <jenniferetracy@gmail.com>
2020-12-04 21:22:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,9 +166,9 @@ end
 
 def big_shoe_rebounds
   largest_shoe = 0 
-  shoe_rebound = -1
-  players(player_name)[:rebounds]>largest_shoe
-  players(player_name)[:rebounds] = largest_shoe
+ 
+  players(player_name)[:shoe]>largest_shoe
+  players(player_name)[:shoe] = largest_shoe
   
-  players[player_name][:shoe]
+  players[player_name][:rebounds]
 end  
